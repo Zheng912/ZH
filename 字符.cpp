1dbd59c9e05b2962182c3ac2c7010a91a804c703
Zheng912 <72786963+Zheng912@users.noreply.github.com>
2020-12-27 23:20:13 +0800

    Update 字符.cpp

--- 字符.cpp
+++ 字符.cpp
@@ -3,13 +3,13 @@
 int main(void)
 {
 	int i;
-	float x;
+	float n;
 	
 	i = 40;
-	x = 839.21f;
+	n = 839.21f;
 	
 	printf("|%d|%5d|%-5d|%5.3d|\n", i, i, i, i);
-	printf("|%10.3f|%10.3e|%-10g|\n", x, x, x);
+	printf("|%10.3f|%10.3e|%-10g|\n", n, n, m);
 	
 	return 0;
  } 
